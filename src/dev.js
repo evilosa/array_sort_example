f8df16d3b1c76353b218921506d6edb0db31786b
Bozhkov Aleksandr <bojkov@sorc.ru>
2019-01-25 08:16:00 +0300

    Add missed new line

--- src/dev.js
+++ src/dev.js
@@ -10,6 +10,7 @@ const runCase = (number = 1, source) => {
   const result = sortString(source)
   logger.info(`Result: ${result}`)
 }
+
 try {
   runCase(1, '103 123 4444 99 2000')
   runCase(2, '2000 10003 1234000 44444444 9999 11 11 22 123')
